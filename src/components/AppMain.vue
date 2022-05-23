2017d9259f004003ef21ed25a149fe120352bd7f
vittorioaquilino <vittorioaquilino17@gmail.com>
2022-05-23 16:44:10 +0200

    add flags

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,10 +5,13 @@
       v-for="(element, index) in cardList"
       :key="index"
       >
-        <p>Titolo originale: {{ element.original_title }}</p>
-        <p>Lingua: {{ element.original_language }}</p>
-        <p>Titolo: {{ element.title }}</p>
-        <p>Voto: {{ element.vote_average }}</p>
+        <div>Titolo originale: {{ element.original_title }}</div>
+        <div>
+            Lingua: {{ element.original_language }}
+            <img :src="isLanguage(element)" alt="">
+        </div>
+        <div>Titolo: {{ element.title }}</div>
+        <div>Voto: {{ element.vote_average }}/10</div>
       </div>
   </section>
 </template>
@@ -16,7 +19,28 @@
 <script>
 export default {
     name: "AppMain",
+    data() {
+        return {
+            italiaFlag: require("../assets/img/italia-flag.jpg"),
+            inghilterraFlag: require("../assets/img/inghilterra-flag.jpg"),
+            germaniaFlag: require("../assets/img/germania-flag.jpg"),
+            franciaFlag: require("../assets/img/francia-flag.jpg")
+        };
+    },
     props: ["cardList"],
+    methods: {
+        isLanguage(element) {
+            if (element.original_language === "it") {
+                return this.italiaFlag;
+            } else if (element.original_language === "en") {
+                return this.inghilterraFlag;
+            } else if (element.original_language === "de") {
+                return this.germaniaFlag;
+            } else if (element.original_language === "fr") {
+                return this.franciaFlag;
+            }
+        }
+    }
 }
 </script>
 
@@ -33,8 +57,16 @@ export default {
     width: calc(100% / 4 - 20px);
     margin: 10px;
     border: 2px solid $brand-title-color;
+    border-radius: 5px;
     color: white;
     font-size: .8rem;
+
+    img {
+        width: 15px;
+        height: 15px;
+        border-radius: 5px;
+    }
 }
+
 
 </style>
