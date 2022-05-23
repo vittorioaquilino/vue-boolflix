21f582626ece079ddd305b6e51cfcbd4a1527579
vittorioaquilino <vittorioaquilino17@gmail.com>
2022-05-23 14:34:20 +0200

    add header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,60 @@
+<template>
+  <section class="ms_header d-flex px-4">
+      <div class="ms_title fw-bold">boolflix</div>
+      <div class="ms_header-search">
+         <input 
+         type="text" 
+         placeholder="cerca un film o una serie TV"
+         class="px-4 text-center"
+         v-model="searchWord">
+        <button class="px-4" @click="$emit('searchEvents', searchWord)">cerca</button>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+    name: "AppHeader",
+    data() {
+        return {
+            searchWord: "",
+        }
+    },
+    methods: {
+        searchMovieOrSeries () {
+            return console.log("film o serie tv");
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../style/variables.scss";
+
+.ms_header {
+    justify-content: space-between;
+    align-items: center;
+    height: 100px;
+    background-color: black;
+
+    .ms_title {
+        color: $brand-title-color;
+        text-transform: uppercase;
+        font-size: 1.5rem;
+    }
+
+    .ms_header-search {
+        
+        input {
+            margin: 0 .7rem;  
+            width: 300px;
+            border: none;
+        }
+
+        button {
+            border: none;
+            cursor: pointer;
+        }
+    }
+}
+</style>
