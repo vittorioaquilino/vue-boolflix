98f4cb1e098cfcd2e6eda9b37d6dfb93fe49df9a
vittorioaquilino <vittorioaquilino17@gmail.com>
2022-05-23 15:49:42 +0200

    milestone 1 completed

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,38 @@
 <template>
     <div id="app">
+        <!-- header -->
         <AppHeader @searchEvents="filterResearch($event)"/>
+        <!-- /header -->
+
+        <!-- main -->
+        <AppMain :cardList="searchMovies"/>
+        <!-- /main -->
     </div>
 </template>
 
 <script>
 import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import axios from "axios";
 
 export default {
   name: 'App',
   components: {
     AppHeader,
+    AppMain,
     
+  },
+  data() {
+    return {
+      searchMovies: []
+    }
   },
   methods: {
     filterResearch(searchWord) {
-       console.log(searchWord);
+      axios.get("https://api.themoviedb.org/3/search/movie?api_key=2446d8ad240c37a38739ddeb5eba1083&query=" + searchWord)
+      .then((resp) => {
+        this.searchMovies = resp.data.results;
+      })
     }
   }
 }
---
+++ src/components/AppMain.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="ms_container">
+      <div 
+      class="movie-card text-center"
+      v-for="(element, index) in cardList"
+      :key="index"
+      >
+        <p>Titolo originale: {{ element.original_title }}</p>
+        <p>Lingua: {{ element.original_language }}</p>
+        <p>Titolo: {{ element.title }}</p>
+        <p>Voto: {{ element.vote_average }}</p>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+    name: "AppMain",
+    props: ["cardList"],
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../style/variables.scss";
+
+.ms_container {
+    width: 80%;
+    margin: 2rem auto;
+    display: flex;
+    flex-wrap: wrap;
+}
+.movie-card {
+    width: calc(100% / 4 - 20px);
+    margin: 10px;
+    border: 2px solid $brand-title-color;
+    color: white;
+    font-size: .8rem;
+}
+
+</style>
